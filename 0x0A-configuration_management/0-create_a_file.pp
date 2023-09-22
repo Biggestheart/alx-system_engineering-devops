03cbae8fef5c94f18a980e6311bdd49717754625
Biggestheart <arrhnnie@gmail.com>
2023-09-22 15:48:04 -0700

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This_anifest creates_a _file_at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
