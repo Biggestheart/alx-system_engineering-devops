24bbed3f2d6b241d3f5882999d5b0bf53557ba5c
Biggestheart <arrhnnie@gmail.com>
2023-11-07 19:40:04 -0800

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fixing_wordpress site_running on_apache2
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
