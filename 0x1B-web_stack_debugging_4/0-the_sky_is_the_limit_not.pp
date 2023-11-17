54a9d879035d0c781e28d2ed52e6a8e0617d8c52
Biggestheart <arrhnnie@gmail.com>
2023-11-16 18:51:10 -0800

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,12 @@
-# Fix nginx to many failed requests
-exec {'increase_workers':
-command => '/bin/sed -i "s/worker_processes 4/worker_processes 8/g" /etc/nginx/nginx.conf',
-notify  => Service['nginx'],
+# fix_limit_files.
+
+exec { 'fix_limit_hbton_user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-service {'nginx':
-ensure => 'running',
+# Increase_soft file_limit hbton ser.
+exec { 'increase_soft_file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
